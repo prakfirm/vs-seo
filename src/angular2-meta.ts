979f604bd94ab8692719c1eccebc64df9c96deed
Pramod Kumar Raghav <praghav@adobe.com>
2017-03-30 10:49:20 +0530

    Updated the angular2-meta to support the json-ld

--- src/angular2-meta.ts
+++ src/angular2-meta.ts
@@ -6,10 +6,10 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Injectable, Inject} from '@angular/core';
+import {Inject, Injectable} from "@angular/core";
 // es6-modules are used here
-import {DomAdapter, getDOM} from '@angular/platform-browser/src/dom/dom_adapter';
-import {DOCUMENT} from '@angular/platform-browser';
+import {DomAdapter, getDOM} from "@angular/platform-browser/src/dom/dom_adapter";
+import {DOCUMENT} from "@angular/platform-browser";
 
 /**
  * Represent meta element.
@@ -35,16 +35,16 @@ import {DOCUMENT} from '@angular/platform-browser';
  * @experimental
  */
 export interface MetaDefinition {
-  charset?: string;
-  content?: string;
-  httpEquiv?: string;
-  id?: string;
-  itemprop?: string;
-  name?: string;
-  property?: string;
-  scheme?: string;
-  url?: string;
-  [prop: string]: string;
+    charset?: string;
+    content?: string;
+    httpEquiv?: string;
+    id?: string;
+    itemprop?: string;
+    name?: string;
+    property?: string;
+    scheme?: string;
+    url?: string;
+    [prop: string]: string;
 }
 
 /**
@@ -54,172 +54,208 @@ export interface MetaDefinition {
  */
 @Injectable()
 export class Meta {
-  private _dom: DomAdapter = getDOM();
+    private _dom: DomAdapter = getDOM();
 
-  constructor( @Inject(DOCUMENT) private _document: any) { }
-  /**
-   * Sets the title of the page
-   */
-  setTitle(title: string) {
-    this._document.title = title
-  }
+    constructor(@Inject(DOCUMENT) private _document: any) {
+    }
 
-  /**
-   * this.meta.updateMeta('description', 'test'); will  set <meta name="description" content="test">
-   */
-  updateMeta(name, content) {
-    const head = this._document.head;
-    let childNodesAsList = this._dom.childNodesAsList(head);
-    let metaEl = childNodesAsList.find(el => el['attribs'] ? el['attribs'].name == name : false);
-    if (metaEl) metaEl['attribs'].content = content;
-  }
+    /**
+     * Sets the title of the page
+     */
+    setTitle(title: string) {
+        this._document.title = title
+    }
 
-  /**
-   * Adds a new meta tag to the dom.
-   *
-   *  ### Example
-   *
-   * ```ts
-   * const name: MetaDefinition = {name: 'application-name', content: 'Name of my application'};
-   * const desc: MetaDefinition = {name: 'description', content: 'A description of the page'};
-   * const tags: HTMLMetaElement[] = this.meta.addTags([name, desc]);
-   * ```
-   *
-   * @param tags
-   * @returns {HTMLMetaElement[]}
-   */
-  addTags(...tags: Array<MetaDefinition|MetaDefinition[]>): HTMLMetaElement[] {
-    const presentTags = this._flattenArray(tags);
-    if (presentTags.length === 0) return [];
-    return presentTags.map((tag: MetaDefinition) => this._addInternal(tag));
-  }
+    /**
+     * this.meta.updateMeta('description', 'test'); will  set <meta name="description" content="test">
+     */
+    updateMeta(name, content) {
+        const head = this._document.head;
+        let childNodesAsList = this._dom.childNodesAsList(head);
+        let metaEl = childNodesAsList.find(el => el['attribs'] ? el['attribs'].name == name : false);
+        if (metaEl) metaEl['attribs'].content = content;
+    }
 
-  /**
-   * Gets the meta tag by the given selector. Returns element or null
-   * if there's no such meta element.
-   *
-   *  ### Example
-   *
-   * ```ts
-   * const meta: HTMLMetaElement = this.meta.getTag('name=description');
-   * const twitterMeta: HTMLMetaElement = this.meta.getTag('name="twitter:title"');
-   * const fbMeta: HTMLMetaElement = this.meta.getTag('property="fb:app_id"');
-   * ```
-   *
-   * @param selector
-   * @returns {HTMLMetaElement}
-   */
-  getTag(selector: string): HTMLMetaElement {
-    if (!selector) return null;
-    return this._dom.query(`meta[${selector}]`);
-  }
 
-  /**
-   * Updates the meta tag with the given selector.
-   *
-   * *  ### Example
-   *
-   * ```ts
-   * const meta: HTMLMetaElement = this.meta.updateTag('name=description', {name: 'description',
+
+
+    /**
+     * Adds a new meta tag to the dom.
+     *
+     *  ### Example
+     *
+     * ```ts
+     * const name: MetaDefinition = {name: 'application-name', content: 'Name of my application'};
+     * const desc: MetaDefinition = {name: 'description', content: 'A description of the page'};
+     * const tags: HTMLMetaElement[] = this.meta.addTags([name, desc]);
+     * ```
+     *
+     * @param tags
+     * @returns {HTMLMetaElement[]}
+     */
+    addTags(...tags: Array<MetaDefinition | MetaDefinition[]>): HTMLMetaElement[] {
+        const presentTags = this._flattenArray(tags);
+        if (presentTags.length === 0) return [];
+        return presentTags.map((tag: MetaDefinition) => this._addInternal(tag));
+    }
+
+    /**
+     * Gets the meta tag by the given selector. Returns element or null
+     * if there's no such meta element.
+     *
+     *  ### Example
+     *
+     * ```ts
+     * const meta: HTMLMetaElement = this.meta.getTag('name=description');
+     * const twitterMeta: HTMLMetaElement = this.meta.getTag('name="twitter:title"');
+     * const fbMeta: HTMLMetaElement = this.meta.getTag('property="fb:app_id"');
+     * ```
+     *
+     * @param selector
+     * @returns {HTMLMetaElement}
+     */
+    getTag(selector: string): HTMLMetaElement {
+        if (!selector) return null;
+        return this._dom.query(`meta[${selector}]`);
+    }
+
+    /**
+     * Updates the meta tag with the given selector.
+     *
+     * *  ### Example
+     *
+     * ```ts
+     * const meta: HTMLMetaElement = this.meta.updateTag('name=description', {name: 'description',
    * content: 'New description'});
-   * console.log(meta.content); // 'New description'
-   * ```
-   *
-   * @param selector
-   * @param tag updated tag definition
-   * @returns {HTMLMetaElement}
-   */
-  updateTag(selector: string, tag: MetaDefinition): HTMLMetaElement {
-    const meta: HTMLMetaElement = this.getTag(selector);
-    if (!meta) {
-      // create element if it doesn't exist
-      return this._addInternal(tag);
-    }
-    return this._prepareMetaElement(tag, meta);
-  }
-
-  /**
-   * Removes meta tag with the given selector from the dom.
-   *
-   *  ### Example
-   *
-   * ```ts
-   * this.meta.removeTagBySelector('name=description');
-   * ```
-   *
-   * @param selector
-   */
-  removeTagBySelector(selector: string): void {
-    const meta: HTMLMetaElement = this.getTag(selector);
-    this.removeTagElement(meta);
-  }
-
-  /**
-   * Removes given meta element from the dom.
-   *
-   *  ### Example
-   *  ```ts
-   * const elem: HTMLMetaElement = this.meta.getTag('name=description');
-   * this.meta.removeTagElement(elem);
-   * ```
-   *
-   * @param meta meta element
-   */
-  removeTagElement(meta: HTMLMetaElement): void {
-    if (meta) {
-      this._removeMetaElement(meta);
-    }
-  }
-
-  addCanonicalLink(href: string) {
-    const link: any = this._createElement('link');
-    const tag = {rel: 'canonical', href};
-    this._prepareMetaElement(tag, link);
-    this._appendMetaElement(link);
-    return link;
-  }
-
-  private _addInternal(tag: MetaDefinition): HTMLMetaElement {
-    const meta: HTMLMetaElement = this._createMetaElement();
-    this._prepareMetaElement(tag, meta);
-    this._appendMetaElement(meta);
-    return meta;
-  }
-
-  private _createMetaElement(): HTMLMetaElement {
-    return this._dom.createElement('meta') as HTMLMetaElement;
-  }
-
-  private _createElement(tag: string): HTMLElement {
-    return this._dom.createElement(tag) as HTMLElement;
-  }
-
-  private _prepareMetaElement(tag: any, el: HTMLElement): any {
-    Object.keys(tag).forEach((prop: string) => this._dom.setAttribute(el, prop, tag[prop]));
-    return el;
-  }
-
-  private _appendMetaElement(el: HTMLElement): void {
-    const head = this._document.head;
-    this._dom.appendChild(head, el);
-  }
-
-  private _removeMetaElement(meta: HTMLMetaElement): void {
-    const head = this._dom.parentElement(meta);
-    this._dom.removeChild(head, meta);
-  }
-
-  private _flattenArray(input: any[], out: any[] = []): any[] {
-    if (input) {
-      for (let i = 0; i < input.length; i++) {
-        const item: any = input[i];
-        if (Array.isArray(item)) {
-          this._flattenArray(item, out);
-        } else if (item) {
-          out.push(item);
+     * console.log(meta.content); // 'New description'
+     * ```
+     *
+     * @param selector
+     * @param tag updated tag definition
+     * @returns {HTMLMetaElement}
+     */
+    updateTag(selector: string, tag: MetaDefinition): HTMLMetaElement {
+        const meta: HTMLMetaElement = this.getTag(selector);
+        if (!meta) {
+            // create element if it doesn't exist
+            return this._addInternal(tag);
         }
-      }
+        return this._prepareMetaElement(tag, meta);
     }
-    return out;
-  }
+
+    /**
+     * Removes meta tag with the given selector from the dom.
+     *
+     *  ### Example
+     *
+     * ```ts
+     * this.meta.removeTagBySelector('name=description');
+     * ```
+     *
+     * @param selector
+     */
+    removeTagBySelector(selector: string): void {
+        const meta: HTMLMetaElement = this.getTag(selector);
+        this.removeTagElement(meta);
+    }
+
+    /**
+     * Removes given meta element from the dom.
+     *
+     *  ### Example
+     *  ```ts
+     * const elem: HTMLMetaElement = this.meta.getTag('name=description');
+     * this.meta.removeTagElement(elem);
+     * ```
+     *
+     * @param meta meta element
+     */
+    removeTagElement(meta: HTMLMetaElement): void {
+        if (meta) {
+            this._removeMetaElement(meta);
+        }
+    }
+
+    /**
+     * this.meta.updateMeta('description', 'test'); will  set <meta name="description" content="test">
+     */
+    addJSONLD(data: any) {
+        const oScript: any = this._createScriptElement('script');
+        oScript.text = 'abced';
+        const tag = {type: 'application/json-ld', id: 'json-ld'};
+
+        this._prepareMetaElement(tag, oScript);
+        this._appendScriptElement(oScript, data);
+        return oScript;
+    }
+
+    addCanonicalLink(href: string) {
+        const link: any = this._createElement('link');
+        const tag = {rel: 'canonical', href};
+        this._prepareMetaElement(tag, link);
+        this._appendMetaElement(link);
+        return link;
+    }
+
+    private _addInternal(tag: MetaDefinition): HTMLMetaElement {
+        const meta: HTMLMetaElement = this._createMetaElement();
+        this._prepareMetaElement(tag, meta);
+        this._appendMetaElement(meta);
+        return meta;
+    }
+
+    private _createMetaElement(): HTMLMetaElement {
+        return this._dom.createElement('meta') as HTMLMetaElement;
+    }
+
+    private _createElement(tag: string): HTMLElement {
+        return this._dom.createElement(tag) as HTMLElement;
+    }
+
+    private _createScriptElement(tag: string): HTMLScriptElement {
+        return this._dom.createElement(tag) as HTMLScriptElement;
+    }
+
+    private _prepareMetaElement(tag: any, el: HTMLElement): any {
+        Object.keys(tag).forEach((prop: string) => this._dom.setAttribute(el, prop, tag[prop]));
+        return el;
+    }
+
+    private _prepareScriptElement(tag: any, el: HTMLElement): any {
+        Object.keys(tag).forEach((prop: string) => this._dom.setAttribute(el, prop, tag[prop]));
+        return el;
+    }
+
+
+    private _appendMetaElement(el: HTMLElement): void {
+        const head = this._document.head;
+        this._dom.appendChild(head, el);
+    }
+
+    private _appendScriptElement(el: HTMLScriptElement, data): void {
+        const head = this._document.head;
+        const code = JSON.stringify(data);
+        const textNode = this._dom.createTextNode(code);
+        this._dom.appendChild(el, textNode);
+        this._dom.appendChild(head, el);
+    }
+
+    private _removeMetaElement(meta: HTMLMetaElement): void {
+        const head = this._dom.parentElement(meta);
+        this._dom.removeChild(head, meta);
+    }
+
+    private _flattenArray(input: any[], out: any[] = []): any[] {
+        if (input) {
+            for (let i = 0; i < input.length; i++) {
+                const item: any = input[i];
+                if (Array.isArray(item)) {
+                    this._flattenArray(item, out);
+                } else if (item) {
+                    out.push(item);
+                }
+            }
+        }
+        return out;
+    }
 }
